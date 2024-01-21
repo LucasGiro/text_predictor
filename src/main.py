1996d3a762a7892470a63862f7ef9420b81140ba
LucasGiro <lucasmaybo@gmail.com>
2024-01-21 16:59:19 -0300

    testeo de algunas funciones de python

--- src/main.py
+++ src/main.py
@@ -136,7 +136,7 @@ def predecir(map_palabras: dict, map_indices: dict, frase_a_predecir: str, n_pal
     # busco la palabra que mas apariciones tuvo durante el proceso de prediccion y reemplazo el guion bajo con esa palabra
     return frase_a_predecir.replace("_", palabra_con_mas_apariciones(apariciones, predicciones))
 
-def main() -> int:
+def main() -> None:
 
     texto_sanitizado = ""
 
@@ -172,4 +172,64 @@ def main() -> int:
     
 
 if __name__ == "__main__":
-    main()             
+    main()
+
+## TESTS ##
+
+def test_palabra_con_mas_apariciones():
+
+    apariciones = { 'programa': 4, 'python': 1, 'aplicacion': 2, 'sistema':3 }
+    predicciones = set()
+    predicciones.update(['programa', 'sistema'])
+
+    assert palabra_con_mas_apariciones(apariciones, predicciones) == 'programa'
+
+def test_es_prediccion_derecha_valida():
+    indice_palabra_base = 2
+    indice_palabra_predecida = 6
+    map_indices = { 0: 'esto', 1: 'es', 2: 'un', 3: 'caso', 4: 'de', 5: 'test', 6: 'en', 7:'python' }
+
+    assert es_prediccion_derecha_valida(indice_palabra_base, indice_palabra_predecida, map_indices) == True
+
+    indice_palabra_base = 0
+    indice_palabra_predecida = 3
+    map_indices = { 0: 'hola', 1: 'mundo', 2: '-', 3: 'esto', 4: 'es', 5: 'un', 6: 'test' }
+
+    assert es_prediccion_derecha_valida(indice_palabra_base, indice_palabra_predecida, map_indices) == False
+
+def test_es_prediccion_izquierda_valida():
+    indice_palabra_base = 6
+    indice_palabra_predecida = 2
+    map_indices = { 0: 'esto', 1: 'es', 2: 'un', 3: 'caso', 4: 'de', 5: 'test', 6: 'en', 7:'python' }
+
+    assert es_prediccion_izquierda_valida(indice_palabra_base, indice_palabra_predecida, map_indices) == True
+
+    indice_palabra_base = 3
+    indice_palabra_predecida = 0
+    map_indices = { 0: 'hola', 1: 'mundo', 2: '-', 3: 'esto', 4: 'es', 5: 'un', 6: 'test' }
+
+    assert es_prediccion_izquierda_valida(indice_palabra_base, indice_palabra_predecida, map_indices) == False
+
+def test_get_predicciones_derecha():
+    ## "hoy haremos los casos de test en python manana haremos los de c"
+    ## haremos los casos de _
+    palabra = 'casos'
+    distancia = 2
+    map_palabras = { 'hoy': [0], 'haremos': [1, 9], 'los': [2, 10], 'casos': [3], 'de': [4, 11], 'test': [5], 'en': [6], 'python': [7], 'manana': [8], 'c': [12] }
+    map_indices = { 0: 'hoy', 1: 'haremos', 2: 'los', 3: 'casos', 4: 'de', 5: 'test', 6: 'en', 7: 'python', 8: 'manana', 9: 'haremos', 10: 'los', 11: 'de', 12: 'c' }
+    n_palabras = 13
+    apariciones = {}
+
+    assert get_predicciones_derecha(palabra, distancia, map_palabras, map_indices, n_palabras, apariciones) == { 'test' }
+
+def test_get_predicciones_izquierda():
+    ## "hoy haremos los casos de test en python manana haremos los de c"
+    ## _ los casos
+    palabra = 'casos'
+    distancia = 2
+    map_palabras = { 'hoy': [0], 'haremos': [1, 9], 'los': [2, 10], 'casos': [3], 'de': [4, 11], 'test': [5], 'en': [6], 'python': [7], 'manana': [8], 'c': [12] }
+    map_indices = { 0: 'hoy', 1: 'haremos', 2: 'los', 3: 'casos', 4: 'de', 5: 'test', 6: 'en', 7: 'python', 8: 'manana', 9: 'haremos', 10: 'los', 11: 'de', 12: 'c' }
+    n_palabras = 13
+    apariciones = {}
+
+    assert get_predicciones_izquierda(palabra, distancia, map_palabras, map_indices, apariciones) == { 'haremos' }
