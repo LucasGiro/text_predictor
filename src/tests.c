de184a6d4b084d048d6e8ab30cacd5e5367fde1d
LucasGiro <lucasmaybo@gmail.com>
2024-02-06 10:04:28 -0300

    agregado de tests para c

--- src/tests.c
+++ src/tests.c
@@ -25,12 +25,16 @@ void test_get_texto_sanitizado() {
     char *result_01 = get_texto_sanitizado(path_01);
     char *path_02 = "./src/archivos_para_testing/test02.txt";
     char *result_02 = get_texto_sanitizado(path_02);
+    char *path_03 = "./src/archivos_para_testing/test03.txt";
+    char *result_03 = get_texto_sanitizado(path_03);
 
     assert(!strcmp(result_01, "esto es una prueba\nesto es una prueba\nhola mundo\nesto es un test"));
     assert(!strcmp(result_02, "y entonces y cure mis heridas\nrezo rezo rezo rezo\n"));
+    assert(!strcmp(result_03, "esto es un texto para probar \nprobando el sanitizador de texto\ntesteando el sanitizador de texto \n"));
     
     free(result_01);
     free(result_02);
+    free(result_03);
 
 }
 
